d1f3b83ab2abbdfd5a7c18a61bc74407f6df5d23
pympimp <dumped.112@gmail.com>
2023-08-06 21:01:08 +0700

    add white space to Agent Page

--- lib/screen/Agent.dart
+++ lib/screen/Agent.dart
@@ -5,16 +5,16 @@ import 'package:firstflutter/screen/Agents.dart';
 class Agent extends StatefulWidget {
   static const routeName = '/Agent';
 
-  static Future<void> NavigatorPush(BuildContext context) async{
+  static Future<void> NavigatorPush(BuildContext context) async {
     return Navigator.push<void>(
       context,
-      MaterialPageRoute(builder: (_)=>Agent()
-      ),
-      );   
-  } 
+      MaterialPageRoute(builder: (_) => Agent()),
+    );
+  }
+
   var agentsName;
   Agent({super.key, this.agentsName});
-  
+
   @override
   State<Agent> createState() => _AgentState();
 }
@@ -23,92 +23,78 @@ class _AgentState extends State<Agent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(124, 57, 232, 1),
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            // ให้กลับไปที่หน้าที่มีชื่อว่า HomePage ด้วยการใช้ Navigator.popUntil()
-            Navigator.popUntil(context, ModalRoute.withName('/')); // กลับไปที่หน้าหลัก (HomePage.dart)
-          },
-        ),
-      ),
-    
-      body: Stack(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: const BackButton(
+            color: Color.fromARGB(255, 255, 255, 255),
+          )),
+      body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.zero,
-            height: MediaQuery.of(context).size.height * 0.48,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Color.fromRGBO(124, 57, 232, 1),
+          Padding(
+            padding: const EdgeInsets.only(top: 15, bottom: 15),
+            child: Image.network(
+              'https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png',
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 67,
-            left: 30,
-            child: Text(
-              'Gecko',
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-          Positioned(
-            bottom: 30,
-            left: 30,
+          Expanded(
             child: Container(
-              padding: EdgeInsets.all(7.0),
+              padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
               decoration: BoxDecoration(
-                color: Color.fromRGBO(199, 244, 90, 1),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              
-              child: Text(
-                'Initiator',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: const Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left:10),
+                        child: Text(
+                          'Description',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 10),
+                    child: Text(
+                      'Gekko the Angeleno leads a tight-knit crew of calamitous creatures. His buddies bound forward, scattering enemies out of the way, with Gekko chasing them down to regroup and go again',
+                      style: TextStyle(fontSize: 15.5),
+                      
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10,top: 10),
+                    child: Row(
+                      children: [
+                        Text('Skill', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          
-          // สามารถเพิ่ม Widget อื่นๆ ที่ต้องการแสดงบน Container พื้นหลังได้ตรงนี้
+          )
         ],
       ),
     );
   }
 }
 
-class Skills extends StatefulWidget {
-  const Skills({super.key});
+// class Agent extends StatelessWidget {
 
-  @override
-  State<Skills> createState() => _SkillsState();
-}
+//   static const routeName = '/Agent';
 
-class _SkillsState extends State<Skills> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('data')
-      ],
-    );
-  }
-}
+//   const Agent({super.key, this.agentsName}); ({Key? key, required.this.agentsName}) : super(key: key);
+  
+//   final AgentsName agentsName; 
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
